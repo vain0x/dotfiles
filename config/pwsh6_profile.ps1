8a5f0c80a1c059b091cd3249127ff66913e85eaf
vain0x <vainzerox@gmail.com>
2021-04-05 19:19:07 +0900

    dotnetコマンドの入力補完を追加

--- config/pwsh6_profile.ps1
+++ config/pwsh6_profile.ps1
@@ -40,6 +40,15 @@ new-alias nuget ./.nuget/nuget.exe
 new-alias paket ./.paket/paket.exe
 new-alias paket-boot ./.paket/paket.bootstrapper.exe
 
+# [タブ補完を有効にする](https://docs.microsoft.com/ja-jp/dotnet/core/tools/enable-tab-autocomplete#bash)
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
+
 # ------------------------------------------------
 # Node.js
 # ------------------------------------------------

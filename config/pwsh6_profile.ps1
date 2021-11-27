c8803bf3676edd8bcb0e51669bcbf2d950c60466
vain0x <vainzerox@gmail.com>
2021-11-27 15:51:54 +0900

    pwshのプロファイルを更新
    環境の差異を減らすため設定の変更などをしない。

--- config/pwsh6_profile.ps1
+++ config/pwsh6_profile.ps1
@@ -1,26 +1,5 @@
 # pwsh (PowerShell)
 
-# Linux-like env and alias
-new-alias which where.exe
-new-alias grep rg
-new-alias find fd
-
-# GitBash の ~ を展開するときに必要。
-$env:HOME = $env:UserProfile
-
-# ------------------------------------------------
-# UTF-8 を強制する
-# ------------------------------------------------
-
-chcp 65001
-
-$OutputEncoding = New-Object System.Text.Utf8Encoding $False
-
-$PSDefaultParameterValues['*:Encoding'] = "utf8"
-$PSDefaultParameterValues['Out-File:Encoding'] = "utf8"
-
-$env:LESSCHARSET = 'utf-8'
-
 # ------------------------------------------------
 # Git
 # ------------------------------------------------
@@ -53,22 +32,14 @@ Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
 # Node.js
 # ------------------------------------------------
 
-# new-alias n npm` run
+function n() {
+    npm run $args
+}
 
 # ------------------------------------------------
 # その他
 # ------------------------------------------------
 
-# 環境変数を表示する。
-function show-env() {
-    ls env:
-}
-
-# パスを表示する。
-function show-path() {
-    [system.Linq.Enumerable]::distinct($env:PATH.split(@(';'))) | sort
-}
-
-function set-userEnv($name, $value) {
-    [system.environment]::setEnvironmentVariable($name, $value, [environmentVariableTarget]::user)
+function rm-rf() {
+    Remove-Item -Recurse -Force $args
 }
